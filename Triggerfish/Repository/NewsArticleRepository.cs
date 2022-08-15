179ff31abf03bf5928924b7989f20a5903ed7abb
Deepthi Kannoth <dkannoth@amgen.com>
2022-08-15 17:51:56 +0530

    Added style changes for images

--- Triggerfish/Repository/NewsArticleRepository.cs
+++ Triggerfish/Repository/NewsArticleRepository.cs
@@ -17,21 +17,21 @@ namespace Triggerfish.Repository
             {
                 ArticleTitle = "Why demand for in-store contactless payment is here to stay",
                 Description = "The demand for in-store contactless payment technology has increased over the past two years.Learn more about this growing trend and how you can     leverage it.",
-                ImageUrl = "https://i0.wp.com/www.inventiva.co.in/wp-content/uploads/2022/06/shutterstock_744970960.webp?resize=780%2C470&ssl=1",
+                ImageUrl = "https://securecdn.pymnts.com/wp-content/uploads/2019/05/NMI-Contactless-Payments.jpg",
                 ReadTime = "4"
             });
             newsArticles.Add(new NewsArticleModel
             {
                 ArticleTitle = "7 ways to increase ecommerce sales",
                 Description = "Increase ecommerce saleswith these 6 projects to improve your customers's online payment experience.",
-                ImageUrl = "https://i0.wp.com/www.inventiva.co.in/wp-content/uploads/2022/06/shutterstock_744970960.webp?resize=780%2C470&ssl=1",
+                ImageUrl = "https://d2kh7o38xye1vj.cloudfront.net/wp-content/uploads/2018/11/Grow-your-e-commerce-sales.jpg",
                 ReadTime = "4"
             });
             newsArticles.Add(new NewsArticleModel
             {
                 ArticleTitle = "How technology empowers businesses to prepare for the future of commerce",
                 Description = "The future of ecommerce is ever changing. Learn how our future-forwardcommerce technology keeps businesses at the forefront.",
-                ImageUrl = "https://i0.wp.com/www.inventiva.co.in/wp-content/uploads/2022/06/shutterstock_744970960.webp?resize=780%2C470&ssl=1",
+                ImageUrl = "https://www.gomodus.com/hubfs/Blog%20Images/Tech%20Changes%20Business.jpg",
                 ReadTime = "4"
             });
             //return list of articles created
